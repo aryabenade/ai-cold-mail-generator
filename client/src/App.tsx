d98bdddfc14df8836c931df891876307ed358f0a
aryabenade <benadearya@gmail.com>
2026-08-13 20:17:35 +0530

    add LandingPage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,23 @@
+import { Toaster } from "react-hot-toast"
+import { useAuth } from "./context/AuthContext"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import LandingPage from "./pages/LandingPage"
 
 const App = () => {
 
+  const { user, loading } = useAuth()
+
+  if (loading) {
+    return <div className="min-h-screen flex items-center justify-center bg-primary-500">Loading...</div>
+  }
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-primary-500">
-      App hahah
-    </div>
+    <BrowserRouter>
+      <Toaster position="top-right" />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 
 }
--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 interface AuthContextType {
     user: any;
@@ -43,8 +43,12 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
         </AuthContext.Provider>
     )
 
-
-
-
-
 }
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error("useAuth must be used within an AuthProvider");
+    }
+    return context;
+};
